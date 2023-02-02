1abb1e1947bb5101de18d0a5bdfdbb3d92464061
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-02-02 11:45:48 +0100

    componenti

---
+++ my-app/src/Hello.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Message } from "./Message";
+export class Hello extends React.Component{
+    render () {
+        return (
+        <div>
+            <h1>hello world!</h1>
+           <Message />
+        </div>)
+    }
+}
