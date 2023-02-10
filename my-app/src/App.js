a2a19f76629900a1aa5bb18a3de1cb34f9643f02
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-02-10 11:53:07 +0100

    da finire

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Counter1 } from "./Counter1"
 import { Counter3 }from "./Counter3";
 import { Counter4 }from "./Counter4";
+import { CicloDiVita }from "./CicloDiVita";
 
 
 
@@ -15,6 +16,7 @@ export class App extends React.Component{
         <Counter1 />
         <Counter3 />
         <Counter4 />
+        <CicloDiVita />
       </div>
       )
 
