d05c032c59be75b501faa2a49498e9adafb6d63b
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-02-18 10:32:25 +0100

    da finire

---
+++ my-app/src/Welcome.js
@@ -0,0 +1,11 @@
+import React from "react"
+export class Welcome extends React.Component{
+    render () {
+        return (
+        <div class="welcome">
+            <h1>Welcome</h1>
+        </div>
+        )
+  
+  }
+}
