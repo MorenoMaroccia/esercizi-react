de00156de46d6f47d84fa45fff6ae53565589809
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-02-23 15:55:17 +0100

    da finire

--- my-app/src/UseRef.js
+++ my-app/src/UseRef.js
@@ -1,29 +1,56 @@
+// import React,{useState} from "react";
+
+
+// export function CarDetails() {
+//     const [data, setData] = useState({
+//         model: '',
+//         year: '',
+//         color:''
+
+//     })
+
+//     function handleValueChange(event) {
+//         // setData(event.target.value)
+//         const name =event.target.name
+//         const value = event.target.value;
+        
+//         setData({
+//             [name]: value,
+//         })
+
+      
+        
+//     }
+//     return(
+//         <div>
+//             <h1>model, {data.model}</h1>
+//             <h1>year, {data.year}</h1>
+//             <h1>color, {data.color}</h1>
+//             <input value={data.model} onChange={handleValueChange} placeholder='model'></input>
+//             <input value={data.year} onChange={handleValueChange} placeholder='year'></input>
+//             <input value={data.color} onChange={handleValueChange} placeholder='color'></input>
+
+//         </div>
+
+//     )
+// }
+
 import React,{useState} from "react";
 
 
 export function CarDetails() {
-    const [data, setData] = useState({
-        model: '',
-        year: '',
-        color:''
-
-    })
+    const [model, setModel] = useState('Macchina')
 
     function handleValueChange(event) {
-        // setData(event.target.value)
-        const { model, year, color } = event.target;
-
-      
+         setModel(event.target.value)
+       
         
     }
     return(
         <div>
-            <h1>model, {data.model}</h1>
-            <h1>year, {data.year}</h1>
-            <h1>color, {data.color}</h1>
-            <input value={data.model} onChange={handleValueChange} placeholder='model'></input>
-            <input value={data.year} onChange={handleValueChange} placeholder='year'></input>
-            <input value={data.color} onChange={handleValueChange} placeholder='color'></input>
+            <p> {model}</p>
+           
+            <input value={model} onChange={handleValueChange} placeholder='model'></input>
 
         </div>
 
