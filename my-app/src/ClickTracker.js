a558c78c8f8777844a8811ef64658d1102488ba0
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-02-25 15:22:17 +0100

    eventi

--- my-app/src/ClickTracker.js
+++ my-app/src/ClickTracker.js
@@ -1,58 +1,3 @@
-
-// import React from "react";
-
-
-
-
-// export class ClickTracker extends React.Component{
-// //   state = {
-// //     count: this.props.initialValue
-// //   }
-
-// // handleClick = () => {
-// //     if (onsubmit= this.props.name) {
-// //         console.log('this is:', this.props.name2);
-// //     }else if (onclick = this.props.name1) {
-// //         console.log('this is:', this.props.name1);
-// //     }else if (onclick = this.props.name2) {
-// //         console.log('this is:', this.props.name2);
-// //     }
-    
-// //   };
-
-// handleClick = () => {
-//     console.log('this is:', this.props.name);
-  
-//   };
-
-// // handleClick1 = () => {
-// //     console.log('this is:', this.props.name1);
-// //     }
-
-// //     handleClick2 = () => {
-// //         console.log('this is:', this.props.name2);
-// //     };
-
-//   render () {
-//       return (
-//       <div>
-//         {/* <h1>{this.state.count}</h1> */}
-//         <button onClick={this.handleClick} name="bottone1">bottone 1</button>
-//         <button onClick={this.handleClick} name="bottone2">bottone 2</button>
-//         <button onClick={this.handleClick} name="bottone3">bottone 3</button>
-
-//         <h1>{}</h1>
-//       </div>
-//       )
-
-// }
-// }
-
-
-
-// export default ClickTracker
-
-
 import React from "react";
 
 
@@ -60,27 +5,24 @@ import React from "react";
 
 export class ClickTracker extends React.Component{
   state = {
-    button:''
+    name:''
   }
 
-// 
-
-handleClick = (event) => {
-    this.setState({button:event.target.value})
-  
+handleClick = (target) => {
+    this.setState({name:target.name})
+    
   };
-
 
 
   render () {
       return (
       <div>
-        {/* <h1>{this.state.count}</h1> */}
-        <button onClick={this.handleClick} name="button" value="bottone1">bottone 1</button>
-        <button onClick={this.handleClick} name="button" value="bottone2">bottone 2</button>
-        <button onClick={this.handleClick} name="button" value="bottone3">bottone 3</button>
+     
+        <button type="button" onClick={this.handleClick} name="button1" >bottone 1</button>
+        <button type="button" onClick={this.handleClick} name="button2" >bottone 2</button>
+        <button type="button" onClick={this.handleClick} name="button3" >bottone 3</button>
 
-        <h1>{this.state.button}</h1>
+        <h1>{this.state.name}</h1>
       </div>
       )
 
