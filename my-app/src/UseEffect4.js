da6c6d291b7860985e189b2047c1f31aab5d9351
Moreno Maroccia <MorenoMaroccia@g,mail.com>
2023-02-26 22:33:32 +0100

    da fare il 4

--- my-app/src/UseEffect4.js
+++ my-app/src/UseEffect4.js
@@ -1,1 +1,52 @@
+import React, { useEffect, useState } from "react";
 
+
+export function TodoList () {
+    const [data, setData] = useState({
+         item:'',
+        items:['ciao'],
+        
+    })
+        
+           
+            
+        
+    // useEffect(()=> {
+    //     fetch(`https://api.github.com/users/${data.item}`)
+    //     .then((response) => {
+    //      return response.json()
+
+    //     .then((json) => {
+
+    //         console.log(json)
+
+    //         setData(json)
+    //     })
+
+    // })
+
+    // }, [data.item]);
+    
+    function handleInputChange(event) {
+        setData({item:event.target.value})
+    }
+
+    function handleAddItem()  {
+        data.items.push(data.item)
+        setData({items: data.items, item:'' }); 
+
+        }
+
+
+        return(
+
+            <div>
+                <input onChange={handleInputChange} value={item} />
+                <button onClick={handleAddItem}>Add</button>
+                <ul>
+                {data.items.map((item) => <li key={item}>{item}{data.name}</li>) }
+                </ul> 
+            </div>
+           
+        )
+    }
